d2886ddc04b6e1e0a28af2f7c3caaaf197f8e886
akusoff <kirill.voloshin.1998@gmail.com>
2021-07-11 22:14:20 +0300

    Refactoring keyboard input

--- kilo.c
+++ kilo.c
@@ -44,21 +44,34 @@ void enableRawMode(){
     die("tcsetattr");
 }
 
+char editorReadKey() {
+  int nread;
+  char c;
+  while((nread = read(STDIN_FILENO, &c, 1)) != 1){
+    if(nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
+
+/*** input ***/
+
+void editorProcessKeypress(){
+  char c = editorReadKey();
+
+  switch(c) {
+    case CTRL_KEY('q'):
+    exit(0);
+    break;
+  }
+}
+
 /*** init ***/
 
 int main() {
   enableRawMode();
 
-  char c = '\0';
   while(1){
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-      die("read");
-    if(iscntrl(c)){
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == CTRL_KEY('q')) break;
+    editorProcessKeypress();
   }
 
   return 0;
